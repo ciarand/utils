ad0cf689c97ea4f89541070523abd58cc71a53f4
Ciaran Downey <me@ciarand.me>
2014-08-05 14:30:55 -0700

    Update doc_conf

--- doc_conf.php
+++ doc_conf.php
@@ -1,11 +1,23 @@
 <?php require 'vendor/autoload.php';
 
 use Sami\Sami;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iter = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in($dir = __DIR__ . '/src/');
+
+$versions = GitVersionCollection::create($dir)
+    ->add('master', 'master branch');
 
 return new Sami(
-    './src/',
+    $iter,
     [
-        'default_opened_level' => 2,
+        'theme'                => 'enhanced',
+        'versions'             => $versions,
+        'default_opened_level' => 1,
         'build_dir'            => __DIR__.'/docs/%version%',
         'cache_dir'            => __DIR__.'/docs/cache/%version%',
     ]
